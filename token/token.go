f705f6d6aaa61a0746b78a66c13e020b2763cb88
MoBoustta <moboustta6@gmail.com>
2021-03-16 23:08:37 +0100

    define the possible TokenTypes as constants

--- token/token.go
+++ token/token.go
@@ -1,5 +1,31 @@
 package token
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "INDENT"
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
 type tokenType string
 
 type Token struct {
