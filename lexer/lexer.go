8297bcc9b265533053ae5c953ed954d094efda6e
MoBoustta <moboustta6@gmail.com>
2021-03-19 22:55:52 +0100

    Implement ReadChar and NextToken function for the lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,14 +1,19 @@
 package lexer
 
+import (
+	"Interpreter/token"
+)
+
 type Lexer struct {
 	input string
-	position int // Position to current char in src code
+	position int // Position to current  char in input
 	readPosition int // Current reading position in input (after current char)
 	ch byte // Current char under examination
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
@@ -20,4 +25,38 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok = token.Token{}
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token {
+		Type:    tokenType,
+		Literal: string(ch),
+	}
 }
