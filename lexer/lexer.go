5482e9c68fc1258de88b9162a9529e202f99433a
MoBoustta <moboustta6@gmail.com>
2021-03-19 21:51:23 +0100

    define a Lexer DS and the New func that going to return a *Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,23 @@
 package lexer
+
+type Lexer struct {
+	input string
+	position int // Position to current char in src code
+	readPosition int // Current reading position in input (after current char)
+	ch byte // Current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input){
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
