ca1122b03889769d4e3b9bac018a4851b12ea53a
MoBoustta <moboustta6@gmail.com>
2021-03-20 14:29:39 +0100

    Add logic to test the literal value

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -27,7 +27,11 @@ func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokenType wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("tests[%d] - tokenType wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
