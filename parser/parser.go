335c0f449a5b5ad1d955366a57c83eed6bfdd3e1
MoBoustta <moboustta6@gmail.com>
2021-03-31 10:05:22 +0100

    fix the bug -> StackOverFlow error in parseLetStatement

--- parser/parser.go
+++ parser/parser.go
@@ -34,7 +34,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program.Statements = []ast.Statement{}
 
 	for p.curToken.Type != token.EOF {
-		stmt := p.ParseProgram()
+		stmt := p.ParseStatement()
 		if stmt != nil {
 			program.Statements = append(program.Statements, stmt)
 		}
@@ -46,9 +46,47 @@ func (p *Parser) ParseProgram() *ast.Program {
 func (p *Parser) ParseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
-		return p.ParseStatement()
+		return p.parseLetStatements()
 	default:
 		return nil
+	}
+}
+
+func (p *Parser) parseLetStatements() *ast.LetStatement {
+	stmt  := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{ Token: p.curToken,  Value: p.curToken.Literal }
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
 	}
 }
 
