a2b152a33a9b1dee8addcc8c5e10f299932c0518
Mohammad  Boustta <moboustt@e1r10p2.1337.ma>
2021-03-31 11:51:18 +0100

    rename 'user' variable to usr to prevent collides with imported package name os/user

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 func main() {
 
-	user, err := user.Current()
+	usr, err := user.Current()
 	if err != nil { panic(err) }
 
-	fmt.Printf("Hello %s! these is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Hello %s! these is the Monkey programming language!\n", usr.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
