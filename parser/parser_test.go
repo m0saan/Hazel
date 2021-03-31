2247c79f5a1ec76234cc0fde7ef996d43ecab0f5
MoBoustta <moboustta6@gmail.com>
2021-03-31 20:00:33 +0100

    creare a unitest for the return statement parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -14,10 +14,10 @@ func TestLetStatements(t *testing.T) {
    		let  838383;
 `
 	l := lexer.New(input) // Create a lexer instance from the input string
-	p := New(l) // Create a parser instance from the lexer.
+	p := New(l)           // Create a parser instance from the lexer.
 
 	program := p.ParseProgram()
-	checkParserErrors(t,p)
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -65,7 +65,7 @@ func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
 	return true
 }
 
-func checkParserErrors(t *testing.T, p *Parser)  {
+func checkParserErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
 
 	if len(errors) == 0 {
@@ -78,4 +78,36 @@ func checkParserErrors(t *testing.T, p *Parser)  {
 	}
 
 	t.FailNow()
+}
+
+func TestReturnStatements(t *testing.T) {
+
+	input := `
+		return 5;
+   		return 10;
+   		return 993322; `
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral())
+		}
+	}
 }
